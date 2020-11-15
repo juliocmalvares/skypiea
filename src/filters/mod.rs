cc59b465eb759c20e71127e73a6252c4530095c1
Julio Álvares <juliocmalvares07@gmail.com>
2020-11-15 00:43:02 -0300

    fix: :bug: FIXING MODULE BUGS
    Fixing the bugs that make the modules inaccessible
    
    Bug Mod

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -1,6 +1,21 @@
 pub mod convolution;
+
 pub mod grayscale;
+pub use self::grayscale::*;
+
 pub mod brightness;
+pub use self::brightness::*;
+
 pub mod threshold;
+pub use self::threshold::*;
+
 pub mod grayscale_luminosity;
+pub use self::grayscale_luminosity::*;
+
+pub mod contrast;
+pub use self::contrast::*;
+
+
+pub mod invert;
+pub use self::invert::*;
 // use crate::io::*;
---
+++ src/io/mod.rs
@@ -0,0 +1,2 @@
+pub mod fastbitmap;
+pub use self::fastbitmap::*;
